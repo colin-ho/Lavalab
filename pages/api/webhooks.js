5760891eefbdc2f3022f8ccabebbafe69addf0cb
Colin Ho <colinho@Colins-MacBook-Pro.local>
2022-01-27 23:17:01 -0800

    sub handling

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -57,9 +57,11 @@ export default async function handler(req, res) {
         }
         else if (event.type==="invoice.paid"){
             try{
-                const {subscription,period_start,period_end} = event.data.object;
+                const subscription =  event.data.object.subscription;
+                const {start,end} = event.data.object.lines.data[0].period;
+                console.log(event.data.object)
                 const sub = await firestore.collectionGroup('subscribedTo').where('stripeSubscriptionId','==',subscription).get();
-                sub.docs.forEach((doc)=>doc.ref.set({start:new Date(period_start*1000),end:new Date(period_end*1000)},{merge:true}));
+                sub.docs.forEach((doc)=>doc.ref.set({start:new Date(start*1000),end:new Date(end*1000)},{merge:true}));
             }
             catch(err){
                 console.log(`❌ Error message: ${err.message}`);
