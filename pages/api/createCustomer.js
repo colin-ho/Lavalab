7fe8a07bea24a06e51d8f7cde930afb6f79e1cf7
Colin Ho <colinho@Colins-MacBook-Pro.local>
2022-02-02 14:37:48 -0800

    changing stripe

--- pages/api/createCustomer.js
+++ pages/api/createCustomer.js
@@ -4,15 +4,11 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function CreateCustomer(req, res) {
     const { userId,email} = req.body;
-    const userSnapshot = firestore.collection('customers').doc(userId)
-
     const customer = await stripe.customers.create({
         metadata: {
             firebaseUID: userId
         },
         email:email,
     });
-    await userSnapshot.ref.update({ stripeCustomerId: customer.id });
     res.json({ customer: customer });
-    
 }
