a31abdf89af3f02ea3957d4c3f451ec44f5ea696
Colin Ho <colinho@Colins-MacBook-Pro.local>
2022-02-02 21:29:15 -0800

    changing stripe

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -34,28 +34,7 @@ export default async function handler(req, res) {
         console.log('✅ Success:', event.id,event.type);
 
         // 2. Handle event type (add business logic here)
-        if (event.type === 'checkout.session.completed') {
-            try {
-                const metadata = event.data.object.metadata;
-                const businessRef = firestore.collection('businesses').doc(metadata.businessId);
-                const subRef = businessRef.collection('subscriptions').doc(metadata.subscriptionId);
-                const customerRef = subRef.collection('customers').doc(metadata.customerId);
-                const customerSub = firestore.collection('customers').doc(metadata.customerId).collection('subscribedTo').doc(metadata.subscriptionId);
-                const newHistory = firestore.collection('customers').doc(metadata.customerId).collection('history').doc()
-                const batch = firestore.batch();
-                batch.update(businessRef, { totalCustomers: increment(1) })
-                batch.update(subRef, { customerCount: increment(1) });
-                batch.set(customerRef, { uid: metadata.customerId, name: metadata.name, redeeming: false, code: '', currentRef: '' });
-                batch.set(customerSub, { subscriptionTitle: metadata.title, subscriptionId: metadata.subscriptionId, stripeSubscriptionId: event.data.object.subscription, redemptionCount: 0 });
-                batch.set(newHistory, { subscriptionTitle: metadata.title, subscriptionId: metadata.subscriptionId, time: serverTimestamp(), price: metadata.price, business: metadata.business,type:'subscription' })
-                await batch.commit();
-            } catch (err) {
-                console.log(`❌ Error message: ${err.message}`);
-                res.status(400).send(`Webhook Error: ${err.message}`);
-                return;
-            }
-        }
-        else if (event.type==="invoice.payment_succeeded"){
+        if (event.type==="invoice.payment_succeeded"){
             try{
                 if(event.data.object.billing_reason === 'subscription_create') {
                     // The subscription automatically activates after successful payment
